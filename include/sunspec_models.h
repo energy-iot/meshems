ea8a0c447c0c841a25072f454299e5e228460bd9
Liam O'Brien <finderman2@gmail.com>
2025-05-07 22:40:53 -0700

    untested 701 block

--- include/sunspec_models.h
+++ include/sunspec_models.h
@@ -15,7 +15,16 @@
 #define SUNSPEC_COMMON_START (SUNSPEC_BASE_ADDR + 4)
 #define SUNSPEC_INVERTER_START (SUNSPEC_COMMON_START + 66)
 
-// Common Model (1) register offsets
+// WORKING Common Model (1) register offsets
+// #define COMMON_MODEL_ID 0
+// #define COMMON_MODEL_LENGTH 2
+// #define COMMON_MANUFACTURER 4
+// #define COMMON_MODEL 20
+// #define COMMON_OPTIONS 36
+// #define COMMON_VERSION 52
+// #define COMMON_SERIAL 54
+
+// TEST Common Model (1) register offsets
 #define COMMON_MODEL_ID 0
 #define COMMON_MODEL_LENGTH 1
 #define COMMON_MANUFACTURER 2
@@ -28,34 +37,80 @@
 // Inverter Model (701) register offsets
 #define INV_MODEL_ID 0
 #define INV_MODEL_LENGTH 1
-#define INV_STATUS 4 // 4 Inverter State
+#define INV_AC_TYPE 2          // AC Wiring Type
+#define INV_OPERATING_STATE 3  // Operating State
+#define INV_STATUS 4           // Inverter State
+#define INV_GRID_CONNECTION 5  // Grid Connection State
+#define INV_ALARM 6            // Alarm Bitfield (2 registers)
+#define INV_DER_MODE 8         // DER Operational Characteristics (2 registers)
+#define INV_AC_POWER 10        // Active Power
+#define INV_AC_VA 11           // Apparent Power
+#define INV_AC_VAR 12          // Reactive Power
+#define INV_AC_PF 13           // Power Factor
+#define INV_AC_CURRENT 14      // Total AC Current
+#define INV_AC_VOLTAGE_LL 15   // Voltage LL
+#define INV_AC_VOLTAGE_LN 16   // Voltage LN
+#define INV_AC_FREQUENCY 17    // Frequency (2 registers)
 
-#define INV_AC_CURRENT 14
-#define INV_AC_CURRENT_A 45
-#define INV_AC_CURRENT_B 68
-#define INV_AC_CURRENT_C 91
-#define INV_AC_VOLTAGE_AB 46
-#define INV_AC_VOLTAGE_BC 69
-#define INV_AC_VOLTAGE_CA 92
-#define INV_AC_VOLTAGE_AN 47
-#define INV_AC_VOLTAGE_BN 70
-#define INV_AC_VOLTAGE_CN 93
-#define INV_AC_POWER 10
-#define INV_AC_FREQUENCY 17
-#define INV_AC_VA 11
-#define INV_AC_VAR 12
-#define INV_AC_PF 13
+// Energy registers
+#define INV_ENERGY_INJECTED 19    // Total Energy Injected (4 registers)
+#define INV_ENERGY_ABSORBED 23    // Total Energy Absorbed (4 registers)
+#define INV_REACTIVE_INJECTED 27  // Total Reactive Energy Injected (4 registers)
+#define INV_REACTIVE_ABSORBED 31  // Total Reactive Energy Absorbed (4 registers)
 
-#define INV_AC_ENERGY_WH 34
-#define INV_DC_CURRENT 36
-#define INV_DC_VOLTAGE 38
-#define INV_DC_POWER 40
-#define INV_TEMP_CABINET 42
-#define INV_TEMP_SINK 44
-#define INV_TEMP_TRANSFORMER 46
-#define INV_TEMP_OTHER 48
+// Temperature registers
+#define INV_TEMP_AMBIENT 35      // Ambient Temperature
+#define INV_TEMP_CABINET 36      // Cabinet Temperature
+#define INV_TEMP_SINK 37         // Heat Sink Temperature
+#define INV_TEMP_TRANSFORMER 38  // Transformer Temperature
+#define INV_TEMP_IGBT 39         // IGBT/MOSFET Temperature
+#define INV_TEMP_OTHER 40        // Other Temperature
 
-#define INV_STATUS_VENDOR 123
+// Phase L1 registers
+#define INV_AC_POWER_L1 41       // Active Power L1
+#define INV_AC_VA_L1 42          // Apparent Power L1
+#define INV_AC_VAR_L1 43         // Reactive Power L1
+#define INV_AC_PF_L1 44          // Power Factor L1
+#define INV_AC_CURRENT_L1 45     // Current L1
+#define INV_AC_VOLTAGE_L1L2 46   // Phase Voltage L1-L2
+#define INV_AC_VOLTAGE_L1N 47    // Phase Voltage L1-N
+
+// Phase L2 registers
+#define INV_AC_POWER_L2 64       // Active Power L2
+#define INV_AC_VA_L2 65          // Apparent Power L2
+#define INV_AC_VAR_L2 66         // Reactive Power L2
+#define INV_AC_PF_L2 67          // Power Factor L2
+#define INV_AC_CURRENT_L2 68     // Current L2
+#define INV_AC_VOLTAGE_L2L3 69   // Phase Voltage L2-L3
+#define INV_AC_VOLTAGE_L2N 70    // Phase Voltage L2-N
+
+// Phase L3 registers
+#define INV_AC_POWER_L3 87       // Active Power L3
+#define INV_AC_VA_L3 88          // Apparent Power L3
+#define INV_AC_VAR_L3 89         // Reactive Power L3
+#define INV_AC_PF_L3 90          // Power Factor L3
+#define INV_AC_CURRENT_L3 91     // Current L3
+#define INV_AC_VOLTAGE_L3L1 92   // Phase Voltage L3-L1
+#define INV_AC_VOLTAGE_L3N 93    // Phase Voltage L3-N
+
+// Throttling registers
+#define INV_THROTTLE_PCT 110     // Throttling In Pct
+#define INV_THROTTLE_SRC 111     // Throttle Source Information (2 registers)
+
+// Scale factors
+#define INV_SF_CURRENT 113       // Current Scale Factor
+#define INV_SF_VOLTAGE 114       // Voltage Scale Factor
+#define INV_SF_FREQUENCY 115     // Frequency Scale Factor
+#define INV_SF_POWER 116         // Active Power Scale Factor
+#define INV_SF_PF 117            // Power Factor Scale Factor
+#define INV_SF_VA 118            // Apparent Power Scale Factor
+#define INV_SF_VAR 119           // Reactive Power Scale Factor
+#define INV_SF_ENERGY 120        // Active Energy Scale Factor
+#define INV_SF_REACTIVE_ENERGY 121 // Reactive Energy Scale Factor
+#define INV_SF_TEMP 122          // Temperature Scale Factor
+
+// Vendor information
+#define INV_ALARM_INFO 123       // Manufacturer Alarm Info (32 registers)
 
 // Status flags for INV_STATUS
 #define STAT_OFF 0x0001
--- src/sunspec_mapper.cpp
+++ src/sunspec_mapper.cpp
@@ -5,6 +5,18 @@
 // External reference to the Sol-Ark instance
 extern Modbus_SolArkLV solark;
 
+// WORKING ONE
+// void set_sunspec_string(uint16_t* registers, const char* str, uint8_t max_len) {
+//     uint8_t i;
+//     for (i = 0; i < max_len && str[i] != '\0'; i += 2) {
+//         uint16_t val = str[i];
+//         if (i + 1 < max_len && str[i + 1] != '\0') {
+//             val |= (str[i + 1] << 8);
+//         }
+//         registers[i / 2] = val;
+//     }
+// }
+
 // Helper function to set a string in the register map
 void set_sunspec_string(uint16_t* registers, const char* str, uint8_t max_len) {
     uint8_t i;
@@ -40,33 +52,33 @@ void setup_sunspec_models() {
     
     // Set Common Model data
     set_sunspec_string(&holdingRegisters[common_offset + COMMON_MANUFACTURER], "SolArk", 32);
-    set_sunspec_string(&holdingRegisters[common_offset + COMMON_MODEL], "SALV", 32);
-    set_sunspec_string(&holdingRegisters[common_offset + COMMON_OPTIONS], "EMS", 16);
-    set_sunspec_string(&holdingRegisters[common_offset + COMMON_VERSION], "1.0", 16);
+    set_sunspec_string(&holdingRegisters[common_offset + COMMON_MODEL], "SOLARK-LV-MODBUS", 32);
+    set_sunspec_string(&holdingRegisters[common_offset + COMMON_OPTIONS], "NA", 16);
+    set_sunspec_string(&holdingRegisters[common_offset + COMMON_VERSION], "130", 16);
     set_sunspec_string(&holdingRegisters[common_offset + COMMON_SERIAL], "123456", 32);
-    //set_sunspec_string(&holdingRegisters[common_offset + COMMON_DEVICE_ADDR], "1", 16);
     holdingRegisters[common_offset + COMMON_DEVICE_ADDR] = 1;
     
-//     // Set Inverter Model (701) header
-//     uint16_t inverter_offset = common_offset + 66 + 2;  // Common model + 2 for next model header
-//     holdingRegisters[inverter_offset + INV_MODEL_ID] = SUNSPEC_MODEL_INVERTER;
-//     holdingRegisters[inverter_offset + INV_MODEL_LENGTH] = 54;  // Length of model block in 16-bit registers
+    // Set Inverter Model (701) header
+    uint16_t inverter_offset = common_offset + 66 + 2;  // Common model + 2 for next model header
+    holdingRegisters[inverter_offset + INV_MODEL_ID] = SUNSPEC_MODEL_INVERTER;
+    holdingRegisters[inverter_offset + INV_MODEL_LENGTH] = 153;  // Length of model block in 16-bit registers
     
-//     // Initialize all inverter model values to "not implemented"
-//     for (uint16_t i = inverter_offset + 4; i < inverter_offset + 54; i++) {
-//         holdingRegisters[i] = SUNSPEC_NOT_IMPLEMENTED;
-//     }
+    // Initialize all inverter model values to "not implemented"
+    for (uint16_t i = inverter_offset + 2; i < inverter_offset + 153; i++) {
+        holdingRegisters[i] = SUNSPEC_NOT_IMPLEMENTED;
+    }
     
-//     // Set scale factors (these are typically negative powers of 10)
-//     holdingRegisters[inverter_offset + INV_AC_CURRENT + 1] = SCALE_FACTOR_0_01;     // AC current scale factor: -2 (0.01)
-//     holdingRegisters[inverter_offset + INV_AC_VOLTAGE_AB + 1] = SCALE_FACTOR_0_1;   // AC voltage scale factor: -1 (0.1)
-//     holdingRegisters[inverter_offset + INV_AC_POWER + 1] = SCALE_FACTOR_1;          // AC power scale factor: 0 (1)
-//     holdingRegisters[inverter_offset + INV_AC_FREQUENCY + 1] = SCALE_FACTOR_0_01;   // AC frequency scale factor: -2 (0.01)
-//     holdingRegisters[inverter_offset + INV_AC_ENERGY_WH + 1] = SCALE_FACTOR_0_001;  // AC energy scale factor: -3 (0.001)
-//     holdingRegisters[inverter_offset + INV_DC_CURRENT + 1] = SCALE_FACTOR_0_01;     // DC current scale factor: -2 (0.01)
-//     holdingRegisters[inverter_offset + INV_DC_VOLTAGE + 1] = SCALE_FACTOR_0_1;      // DC voltage scale factor: -1 (0.1)
-//     holdingRegisters[inverter_offset + INV_DC_POWER + 1] = SCALE_FACTOR_1;          // DC power scale factor: 0 (1)
-//     holdingRegisters[inverter_offset + INV_TEMP_CABINET + 1] = SCALE_FACTOR_0_1;    // Temperature scale factor: -1 (0.1)
+    // Set scale factors
+    holdingRegisters[inverter_offset + INV_SF_CURRENT] = SCALE_FACTOR_0_01;      // Current scale factor: -2 (0.01)
+    holdingRegisters[inverter_offset + INV_SF_VOLTAGE] = SCALE_FACTOR_0_1;       // Voltage scale factor: -1 (0.1)
+    holdingRegisters[inverter_offset + INV_SF_FREQUENCY] = SCALE_FACTOR_0_01;    // Frequency scale factor: -2 (0.01)
+    holdingRegisters[inverter_offset + INV_SF_POWER] = SCALE_FACTOR_1;           // Power scale factor: 0 (1)
+    holdingRegisters[inverter_offset + INV_SF_PF] = SCALE_FACTOR_0_01;           // Power factor scale factor: -2 (0.01)
+    holdingRegisters[inverter_offset + INV_SF_VA] = SCALE_FACTOR_1;              // Apparent power scale factor: 0 (1)
+    holdingRegisters[inverter_offset + INV_SF_VAR] = SCALE_FACTOR_1;             // Reactive power scale factor: 0 (1)
+    holdingRegisters[inverter_offset + INV_SF_ENERGY] = SCALE_FACTOR_0_001;      // Energy scale factor: -3 (0.001)
+    holdingRegisters[inverter_offset + INV_SF_REACTIVE_ENERGY] = SCALE_FACTOR_0_001; // Reactive energy scale factor: -3 (0.001)
+    holdingRegisters[inverter_offset + INV_SF_TEMP] = SCALE_FACTOR_0_1;          // Temperature scale factor: -1 (0.1)
 }
 
 // Update SunSpec registers with Sol-Ark data
@@ -74,84 +86,82 @@ void update_sunspec_from_solark() {
     uint16_t common_offset = 2;
     uint16_t inverter_offset = common_offset + 66 + 2;  // Common model + 2 for next model header
     
-    // // Update AC measurements
-    // // Total AC current - average of L1 and L2
-    // float ac_current_total = (solark.getInverterCurrentL1() + solark.getInverterCurrentL2()) / 2.0f;
-    // holdingRegisters[inverter_offset + INV_AC_CURRENT] = ac_current_total * 100;  // Scale by 100 for 0.01 scale factor
+    // Set AC wiring type (split phase for typical residential)
+    holdingRegisters[inverter_offset + INV_AC_TYPE] = 1;  // Split phase
     
-    // // Phase currents
-    // holdingRegisters[inverter_offset + INV_AC_CURRENT_A] = solark.getInverterCurrentL1() * 100;
-    // holdingRegisters[inverter_offset + INV_AC_CURRENT_B] = solark.getInverterCurrentL2() * 100;
-    // holdingRegisters[inverter_offset + INV_AC_CURRENT_C] = SUNSPEC_NOT_IMPLEMENTED;  // Not used in this system
+    // Set operating state
+    holdingRegisters[inverter_offset + INV_OPERATING_STATE] = solark.getInverterPower() > 0 ? 1 : 0;  // 0=Off, 1=On
     
-    // // AC voltage
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_AB] = solark.getInverterVoltage() * 10;  // Scale by 10 for 0.1 scale factor
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_BC] = SUNSPEC_NOT_IMPLEMENTED;
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_CA] = SUNSPEC_NOT_IMPLEMENTED;
+    // Set inverter state
+    uint16_t invState = 0;  // OFF
+    if (solark.getInverterPower() > 0) {
+        if (solark.getInverterPower() < 100) {
+            invState = 7;  // STANDBY
+        } else {
+            invState = 3;  // RUNNING
+        }
+    }
+    holdingRegisters[inverter_offset + INV_STATUS] = invState;
     
-    // // Phase-to-neutral voltages (not available in this system)
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_AN] = SUNSPEC_NOT_IMPLEMENTED;
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_BN] = SUNSPEC_NOT_IMPLEMENTED;
-    // holdingRegisters[inverter_offset + INV_AC_VOLTAGE_CN] = SUNSPEC_NOT_IMPLEMENTED;
+    // Set grid connection state
+    holdingRegisters[inverter_offset + INV_GRID_CONNECTION] = solark.isGridConnected() ? 1 : 0;
     
-    // // AC power
-    // holdingRegisters[inverter_offset + INV_AC_POWER] = solark.getInverterPower();
+    // Set alarm bitfield (not implemented in this example)
+    holdingRegisters[inverter_offset + INV_ALARM] = 0;
+    holdingRegisters[inverter_offset + INV_ALARM + 1] = 0;
     
-    // // AC frequency
-    // holdingRegisters[inverter_offset + INV_AC_FREQUENCY] = solark.getInverterFrequency() * 100;  // Scale by 100 for 0.01 scale factor
+    // Set DER operational characteristics
+    uint32_t derMode = 0;
+    derMode |= 0x0001;  // Grid following
+    holdingRegisters[inverter_offset + INV_DER_MODE] = (derMode >> 16) & 0xFFFF;
+    holdingRegisters[inverter_offset + INV_DER_MODE + 1] = derMode & 0xFFFF;
     
-    // // AC energy
-    // // Convert kWh to Wh and scale by 1000 for 0.001 scale factor
-    // float load_energy_wh = solark.getLoadEnergy() * 1000.0f;
-    // holdingRegisters[inverter_offset + INV_AC_ENERGY_WH] = load_energy_wh;
+    // Set power measurements
+    holdingRegisters[inverter_offset + INV_AC_POWER] = solark.getInverterPower();
+    holdingRegisters[inverter_offset + INV_AC_VA] = solark.getInverterPower();  // Approximation
+    holdingRegisters[inverter_offset + INV_AC_VAR] = 0;  // Not available
+    holdingRegisters[inverter_offset + INV_AC_PF] = 100;  // Assuming power factor of 1.0 (scaled by 100)
     
-    // // DC measurements
-    // holdingRegisters[inverter_offset + INV_DC_CURRENT] = solark.getBatteryCurrent() * 100;  // Scale by 100 for 0.01 scale factor
-    // holdingRegisters[inverter_offset + INV_DC_VOLTAGE] = solark.getBatteryVoltage() * 10;   // Scale by 10 for 0.1 scale factor
-    // holdingRegisters[inverter_offset + INV_DC_POWER] = solark.getBatteryPower();
+    // Set current and voltage measurements
+    float ac_current_total = (solark.getInverterCurrentL1() + solark.getInverterCurrentL2()) / 2.0f;
+    holdingRegisters[inverter_offset + INV_AC_CURRENT] = ac_current_total * 100;  // Scale by 100 for 0.01 scale factor
+    holdingRegisters[inverter_offset + INV_AC_VOLTAGE_LL] = solark.getInverterVoltage() * 10;  // Scale by 10 for 0.1 scale factor
+    holdingRegisters[inverter_offset + INV_AC_VOLTAGE_LN] = (solark.getInverterVoltage() / 1.732) * 10;  // Approximation for LN voltage
     
-    // // Temperature
-    // holdingRegisters[inverter_offset + INV_TEMP_CABINET] = solark.getBatteryTemperature() * 10;  // Scale by 10 for 0.1 scale factor
+    // Set frequency (2 registers for uint32)
+    uint32_t frequency = solark.getInverterFrequency() * 100;  // Scale by 100 for 0.01 scale factor
+    holdingRegisters[inverter_offset + INV_AC_FREQUENCY] = (frequency >> 16) & 0xFFFF;
+    holdingRegisters[inverter_offset + INV_AC_FREQUENCY + 1] = frequency & 0xFFFF;
     
-    // // Status
-    // uint16_t status = 0;
+    // Set energy measurements (4 registers each for uint64)
+    uint64_t energy_wh = solark.getLoadEnergy() * 1000;  // Convert kWh to Wh
+    holdingRegisters[inverter_offset + INV_ENERGY_INJECTED] = (energy_wh >> 48) & 0xFFFF;
+    holdingRegisters[inverter_offset + INV_ENERGY_INJECTED + 1] = (energy_wh >> 32) & 0xFFFF;
+    holdingRegisters[inverter_offset + INV_ENERGY_INJECTED + 2] = (energy_wh >> 16) & 0xFFFF;
+    holdingRegisters[inverter_offset + INV_ENERGY_INJECTED + 3] = energy_wh & 0xFFFF;
     
-    // // Determine inverter status based on available data
-    // if (solark.getInverterPower() == 0) {
-    //     status |= STAT_OFF;
-    // } else if (solark.getInverterPower() < 100) {  // Arbitrary low power threshold
-    //     status |= STAT_STANDBY;
-    // } else {
-    //     // Normal operation - could be refined with more detailed status information
-    //     status |= STAT_MPPT;
-    // }
+    // Set temperature measurements
+    holdingRegisters[inverter_offset + INV_TEMP_CABINET] = solark.getBatteryTemperature() * 10;  // Scale by 10 for 0.1 scale factor
     
-    // holdingRegisters[inverter_offset + INV_STATUS] = status;
+    // Set phase L1 measurements
+    holdingRegisters[inverter_offset + INV_AC_POWER_L1] = solark.getLoadPowerL1();
+    holdingRegisters[inverter_offset + INV_AC_CURRENT_L1] = solark.getInverterCurrentL1() * 100;
+    holdingRegisters[inverter_offset + INV_AC_VOLTAGE_L1L2] = solark.getInverterVoltage() * 10;
     
-    // // Vendor-specific status information
-    // uint16_t vendor_status = 0;
-    // if (solark.isGridConnected()) {
-    //     vendor_status |= 0x0001;  // Bit 0: Grid connected
-    // }
-    // if (solark.isGeneratorConnected()) {
-    //     vendor_status |= 0x0002;  // Bit 1: Generator connected
-    // }
-    // if (solark.isBatteryCharging()) {
-    //     vendor_status |= 0x0004;  // Bit 2: Battery charging
-    // }
-    // if (solark.isBatteryDischarging()) {
-    //     vendor_status |= 0x0008;  // Bit 3: Battery discharging
-    // }
-    // if (solark.isSellingToGrid()) {
-    //     vendor_status |= 0x0010;  // Bit 4: Selling to grid
-    // }
-    // if (solark.isBuyingFromGrid()) {
-    //     vendor_status |= 0x0020;  // Bit 5: Buying from grid
-    // }
+    // Set phase L2 measurements
+    holdingRegisters[inverter_offset + INV_AC_POWER_L2] = solark.getLoadPowerL2();
+    holdingRegisters[inverter_offset + INV_AC_CURRENT_L2] = solark.getInverterCurrentL2() * 100;
     
-    // holdingRegisters[inverter_offset + INV_STATUS_VENDOR] = vendor_status;
+    // Vendor-specific status information in the alarm info field
+    char alarmInfo[64] = {0};
+    sprintf(alarmInfo, "Grid:%s Batt:%s", 
+            solark.isGridConnected() ? "Connected" : "Disconnected",
+            solark.isBatteryCharging() ? "Charging" : (solark.isBatteryDischarging() ? "Discharging" : "Idle"));
+    
+    set_sunspec_string(&holdingRegisters[inverter_offset + INV_ALARM_INFO], alarmInfo, 64);
     
     //SunSpec Ending Block
-    holdingRegisters[inverter_offset] = 0xFFFF;
-    holdingRegisters[inverter_offset + 1] = 0x0000;
+    uint16_t end_offset = inverter_offset + 153 + 2;  // After Inverter Model + 2 for next model header
+    holdingRegisters[end_offset] = 0xFFFF;
+    holdingRegisters[end_offset + 1] = 0x0000;
 }
