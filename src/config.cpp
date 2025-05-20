5d8c5ea2a3285c0eacbcaa7a84caf665926ddd76
Doug Mendonca <nesl-admin>
2025-05-19 17:48:50 -0700

    more ID fixes....ugh
    Signed-off-by: Doug Mendonca <nesl-admin>

--- src/config.cpp
+++ src/config.cpp
@@ -9,14 +9,16 @@ char device_id[MAX_DEVICE_ID_CHARS] = {0};
 
 void generatefullDeviceID() {// includes OUI vendorid of the ethernet MAC inside the ESP32S2 -prefer to not use  full MACid
   uint32_t low = ESP.getEfuseMac() & 0xFFFFFFFF;
-  uint32_t high = (ESP.getEfuseMac() >> 32) % 0xFFFFFFFF;
+  uint32_t high = (ESP.getEfuseMac() >> 32) & 0xFFFFFFFF;
   //uint64_t fullMAC = word(low, high);
   sprintf(device_id, "%s%X%X", DEVICE_ID_PREFIX, high, low);
 }
 
 void generateDeviceID() {
-  uint32_t uniquePart = (ESP.getEfuseMac() >> 32) % 0xFFFFFFFF;  // Keep only the lower 4 bytes
-  sprintf(device_id, "%s%X", DEVICE_ID_PREFIX, uniquePart);
+  uint8_t mac[6];
+  esp_read_mac(mac, ESP_MAC_WIFI_STA);
+  //Serial.printf("MAC: %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  sprintf(device_id, "%s%02X%02X%02X", DEVICE_ID_PREFIX, mac[3], mac[4], mac[5]);
 }
 
 const char* getDeviceID() {
