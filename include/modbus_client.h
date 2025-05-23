5f01bafa109350960525fe7c03bf3ab33b222e84
Liam O'Brien <finderman2@gmail.com>
2025-05-23 15:43:20 -0700

    Draft implemention of model 713

--- include/modbus_client.h
+++ include/modbus_client.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <ModbusIP_ESP8266.h>
 
-#define WIFI_SSID "SSID"
-#define WIFI_PASSWORD "PASSWORD"
+// WiFi credentials - these should be defined in a separate header file in practice
+// #define WIFI_SSID "Eris"
+// #define WIFI_PASSWORD "LimaOscarAlpha28!"
+
+#define WIFI_SSID "Port Labs Members"
+#define WIFI_PASSWORD "lesstalkmorewifi"
+
+// #define WIFI_SSID "SSID"
+// #define WIFI_PASSWORD "PASSWORD"
 
 void setup_modbus_client();
 void loop_modbus_client();
