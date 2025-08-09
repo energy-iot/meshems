2c95bd9f8d74938618d5192965190e7f8c757842
Liam O'Brien <finderman2@gmail.com>
2025-08-09 00:58:53 -0700

    changed to slave_id
    Signed-off-by: Liam O'Brien <finderman2@gmail.com>

--- ems/modbus_server.py
+++ ems/modbus_server.py
@@ -24,7 +24,7 @@ class ModbusServerConfig:
     
     host: str = "0.0.0.0"
     port: int = 8502
-    unit_id: int = 1
+    slave_id: int = 1
     device_info: Dict[str, str] = None
 
 
@@ -80,7 +80,7 @@ class SunSpecModbusServer:
         
         # Create server context
         self.server_context = ModbusServerContext()
-        self.server_context[self.config.unit_id] = self.slave_context
+        self.server_context[self.config.slave_id] = self.slave_context
         
         # Initialize SunSpec registers
         self._update_sunspec_registers()
@@ -200,7 +200,7 @@ class SunSpecModbusServer:
             "running": self.is_running(),
             "host": self.config.host,
             "port": self.config.port,
-            "unit_id": self.config.unit_id,
+            "slave_id": self.config.slave_id,
             "register_count": len(self.sunspec_mapper.get_all_registers())
         }
 
@@ -250,7 +250,7 @@ class AsyncSunSpecModbusServer:
         
         # Create server context
         self.server_context = ModbusServerContext()
-        self.server_context[self.config.unit_id] = self.slave_context
+        self.server_context[self.config.slave_id] = self.slave_context
         
         # Initialize SunSpec registers
         self._update_sunspec_registers()
